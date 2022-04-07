8ee8be94bb2728878bd1deb74447ccf88d9f3592
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-07 19:03:56 +0100

    GREEN: StrongPasswordShould  test_has_some_number

--- strong_password.py
+++ strong_password.py
@@ -1,8 +1,12 @@
 import unittest
+import re
 
 def is_strong_password(password: str) -> bool:
 
     if len(password) < 6:
+        return False
+
+    if not (re.findall("[0-9]", password)):
         return False
 
     return True
@@ -16,12 +20,11 @@ class StrongPasswordShould(unittest.TestCase):
         self.assertTrue(is_strong_password("Mario_123"))
 
     def test_has_as_a_minimum_six_characteres(self):
-        self.assertFalse(is_strong_password("23"))
-        self.assertTrue(is_strong_password("12_Mar"))
+        self.assertFalse(is_strong_password("23m_"))
 
     def test_has_some_number(self):
-        self.assertFalse(is_strong_password("123456"))
-        self.assertFalse(is_strong_password("123__123"))
+        self.assertFalse(is_strong_password("dsfjkd__"))
+
 
 
 
