ed83cf39833335bfdb5aac3cd1c1ac0799b1164d
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-07 18:46:43 +0100

    RED: StrongPasswordShould test_happy_test

--- strong_password.py
+++ strong_password.py
@@ -1,9 +1,22 @@
 import unittest
 
+#def is_strong_password(password: str) -> bool:
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+
+
+class StrongPasswordShould(unittest.TestCase):
+
+    def test_happy_test(self):
+        self.assertTrue(is_strong_password("Mario_123"))
+    """
+    def test_has_as_a_minimum_six_characteres(self):
+        
+        
+        self.assertFalse(is_strong_password("23"))
+        self.assertFalse(is_strong_password("12_Mar"))
+    """
+
 
 
 if __name__ == '__main__':
