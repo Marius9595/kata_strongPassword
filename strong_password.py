a77819088c654ca232c28d2db06a2e29a5b81a6c
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-07 19:15:15 +0100

    refactor

--- strong_password.py
+++ strong_password.py
@@ -3,13 +3,15 @@ import re
 
 def is_strong_password(password: str) -> bool:
 
-    if len(password) < 6:
-        return False
+    has_not_the_minimun_number_of_characteres = len(password) < 6
+    has_not_some_number = not(re.findall("[0-9]", password))
+    has_not_capital_letters = not(re.findall("[A-Z]", password))
 
-    if not(re.findall("[0-9]", password)):
+    if has_not_the_minimun_number_of_characteres:
         return False
-
-    if not(re.findall("[A-Z]", password)):
+    if has_not_some_number:
+        return False
+    if has_not_capital_letters:
         return False
 
     return True
